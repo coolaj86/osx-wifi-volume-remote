a6c7426bb4c75d93c624a76584f1079a4ab4082d
AJ ONeal <coolaj86@gmail.com>
2020-10-10 13:54:29 -0600

    npm audit fix --force; fix app.js

--- app.js
+++ app.js
@@ -1,77 +1,82 @@
 #!/usr/bin/env node
 (function () {
-  "use strict";
+    "use strict";
 
-  var connect = require("express"),
-    path = require("path"),
-    app = connect(),
-    wivol = require("./lib/osascript-vol-ctrl"),
-    exec = require("child_process").exec;
-  app.use(connect.staticCache());
-  app.use(connect.compress());
-  app.use(connect.static(path.join(__dirname, "public")));
-  app.use(connect.urlencoded());
-  app.use(connect.json());
-  app.use(app.router);
+    var connect = require("express");
+    var path = require("path");
+    var app = connect();
+    var wivol = require("./lib/osascript-vol-ctrl");
+    var exec = require("child_process").exec;
 
-  // Non-RESTful controls (via GET)
+    //app.use(connect.staticCache());
+    //app.use(connect.compress());
+    app.use(connect.static(path.join(__dirname, "public")));
+    app.use(connect.urlencoded());
+    app.use(connect.json());
+    //app.use(app.router);
 
-  // READ volume level and mute state
-  app.get("/controls/volume", function (req, res) {
-    function respond(err, cur, muted) {
-      res.send({
-        volume: cur,
-        muted: muted,
-      });
-    }
-    wivol.get(respond);
-  });
+    // Non-RESTful controls (via GET)
 
-  // CREATE mute (retains volume)
-  app.get("/controls/mute", function (req, res) {
-    function respond(err, cur, muted) {
-      res.send({
-        volume: cur,
-        muted: muted,
-      });
-    }
-    wivol.mute(respond, 300);
-  });
-
-  // DESTROY mute (retains volume)
-  app.get("/controls/unmute", function (req, res) {
-    function respond(err, cur, muted) {
-      res.send({
-        volume: cur,
-        muted: muted,
-      });
-    }
-    wivol.unmute(respond, 300);
-  });
-
-  // UPDATE volume level (stays muted or unmuted)
-  app.get("/controls/volume/:volume", function (req, res) {
-    function respond(err, cur, muted) {
-      res.send({
-        volume: cur,
-        muted: muted,
-      });
-    }
-    wivol.fade(respond, req.params.volume, 600);
-  });
-
-  module.exports = app;
-
-  if (require.main === module) {
-    exec("hostname", function (err, stdout) {
-      require("http")
-        .createServer(app)
-        .listen(process.argv[2] || 4040, function () {
-          console.log("Visit on your phone at ");
-          console.log(
-            "http://" + stdout.trim() + ":" + this.address().port + "/"
-          );
-        });
+    // READ volume level and mute state
+    app.get("/controls/volume", function (req, res) {
+        function respond(err, cur, muted) {
+            res.send({
+                volume: cur,
+                muted: muted,
+            });
+        }
+        wivol.get(respond);
     });
-  }
+
+    // CREATE mute (retains volume)
+    app.get("/controls/mute", function (req, res) {
+        function respond(err, cur, muted) {
+            res.send({
+                volume: cur,
+                muted: muted,
+            });
+        }
+        wivol.mute(respond, 300);
+    });
+
+    // DESTROY mute (retains volume)
+    app.get("/controls/unmute", function (req, res) {
+        function respond(err, cur, muted) {
+            res.send({
+                volume: cur,
+                muted: muted,
+            });
+        }
+        wivol.unmute(respond, 300);
+    });
+
+    // UPDATE volume level (stays muted or unmuted)
+    app.get("/controls/volume/:volume", function (req, res) {
+        function respond(err, cur, muted) {
+            res.send({
+                volume: cur,
+                muted: muted,
+            });
+        }
+        wivol.fade(respond, req.params.volume, 600);
+    });
+
+    module.exports = app;
+
+    if (require.main === module) {
+        exec("hostname", function (err, stdout) {
+            require("http")
+                .createServer(app)
+                .listen(process.argv[2] || 4040, function () {
+                    console.log("Visit on your phone at ");
+                    console.log(
+                        "http://" +
+                            stdout.trim() +
+                            ":" +
+                            this.address().port +
+                            "/"
+                    );
+                });
+        });
+    }
 })();
